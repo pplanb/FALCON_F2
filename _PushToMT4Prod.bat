b58d1002b0a473c62b4f69e55e2a8ef163cca6a8
pplanb <rolandhablutzel@hotmail.com>
2018-10-01 16:14:46 +0100

    updated path

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,16 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_F2"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\FALCON_F2"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\FALCON_F2"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\FALCON_F2"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\FALCON_F2"
+set SOURCE_DIR="D:\TradingRepos\FALCON_F2"
+set DEST_DIR1="D:\FxPro - Terminal1\MQL4\Experts\FALCON_F2"
+set DEST_DIR2="D:\FxPro - Terminal2\MQL4\Experts\FALCON_F2"
+set DEST_DIR3="D:\FxPro - Terminal3\MQL4\Experts\FALCON_F2"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR3% *.mq4
-ROBOCOPY %SOURCE_DIR% %DEST_DIR4% *.mq4
-
-
-
